b62c49376dafbfa3222068a4413d93fa108816d3
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-02-04 17:08:42 +0530

    Added product listing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-import { NavLink, Route, Switch } from "react-router-dom";
+import { NavLink, Route, Switch, Redirect } from "react-router-dom";
 
-import Home from "./components/Home";
-import PageNotFound from "./components/PageNotFound";
+import PageNotFound from "./components/commons/PageNotFound";
 import Product from "./components/Product";
+import ProductList from "./components/ProductList";
 
 const App = () => (
   <>
@@ -17,8 +17,9 @@ const App = () => (
       </NavLink>
     </div>
     <Switch>
-      <Route exact component={Home} path="/" />
-      <Route exact component={Product} path="/product" />
+      <Route exact component={ProductList} path="/products" />
+      <Route exact component={Product} path="/products/:slug" />
+      <Redirect exact from="/" to="/products" />
       <Route component={PageNotFound} path="*" />
     </Switch>
   </>
