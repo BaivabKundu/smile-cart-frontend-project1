3fd6a1da144d84bfa6250128b75274644c1a687d
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-02-05 18:28:54 +0530

    Utilized babel-preset-neeto to access Zustand store properties

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -13,7 +13,7 @@ import PriceCard from "./PriceCard";
 import ProductCard from "./ProductCard";
 
 const Cart = () => {
-  const { cartItems, setSelectedQuantity } = useCartItemsStore();
+  const { cartItems, setSelectedQuantity } = useCartItemsStore.pick();
 
   const slugs = keys(cartItems);
 
